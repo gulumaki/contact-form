002a16feb52cc1a5fae275cff83f328e80e7c00f
gulumaki <fisherman.0877@gmail.com>
2024-09-18 23:50:54 +0900

    sinki

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ContactController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ContactController::class, 'index']);
+Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::post( '/contacts', [ContactController::class, 'store']);
+
+
+
+
